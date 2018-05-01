dfebbda7e999f7bdeb97d0be77766de0cf67d6ea
lovemyself0 <1040702610@qq.com>
2018-05-01 21:35:31 +0800

    数据mock

---
+++ src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>
+    <li>asdf</li>
+    <li>asdf</li>
+  </ul>
+</template>
+<script type="text/ecmascript-6">
+
+
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+
+
+</style>
