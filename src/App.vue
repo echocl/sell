88043680494d4a4030c38a01d738a30df95e4f29
lovemyself0 <1040702610@qq.com>
2018-05-02 23:36:07 +0800

    路由基础配置

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,44 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods" activeClass="active">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings" activeClass="active">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller" activeClass="active">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
-
 <script>
+import header from './components/header/header.vue';
+
 export default {
-  name: 'App'
-}
-
+  components: {
+    'v-header': header
+  }
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
 </style>
